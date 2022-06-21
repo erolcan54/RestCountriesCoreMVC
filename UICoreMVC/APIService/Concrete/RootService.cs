d1d483c98986753e4c054f01c8f057d67bc5a3c7
erolcan54 <erolcan@live.com>
2022-06-21 14:45:03 +0300

    21/06/2022 14:44 Update

--- UICoreMVC/APIService/Concrete/RootService.cs
+++ UICoreMVC/APIService/Concrete/RootService.cs
@@ -9,7 +9,7 @@ using UICoreMVC.APIService.Abstract;
 
 namespace UICoreMVC.APIService.Concrete
 {
-    public class RootService:IRootService
+    public class RootService : IRootService
     {
         private HttpClient _httpClient;
 
@@ -27,8 +27,8 @@ namespace UICoreMVC.APIService.Concrete
             {
                 var myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(data);
                 var filter = (from item in myDeserializedClass.ToList()
-                    where !string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())
-                    select item).ToList();
+                              where !string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())
+                              select item).ToList();
                 return filter;
             }
             return null;
@@ -43,15 +43,43 @@ namespace UICoreMVC.APIService.Concrete
             {
                 var myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(data);
                 var filter = (from item in myDeserializedClass
-                    where (!string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())) ||
-                          (!string.IsNullOrEmpty(item.name) && item.name.ToLower().Contains(key.ToLower())) ||
-                          (!string.IsNullOrEmpty(item.region) && item.region.ToLower().Contains(key.ToLower())) ||
-                          (item.topLevelDomain.Count>0 && item.topLevelDomain.Contains(key.ToLower())) ||
-                          (!string.IsNullOrEmpty(item.alpha2Code) && item.alpha2Code.ToLower().Contains(key.ToLower())) ||
-                          (!string.IsNullOrEmpty(item.alpha3Code) && item.alpha3Code.ToLower().Contains(key.ToLower())) ||
-                          (item.callingCodes.Count > 0 && item.callingCodes.Contains(key.ToLower()))||
-                          (item.altSpellings.Count > 0 && item.altSpellings.Contains(key.ToLower())) ||
-                          (!string.IsNullOrEmpty(item.subregion) && item.subregion.ToLower().Contains(key.ToLower()))
+                              where (!string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.name) && item.name.ToLower().Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.region) && item.region.ToLower().Contains(key.ToLower())) ||
+                                    (item.topLevelDomain.Count > 0 && item.topLevelDomain.Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.alpha2Code) && item.alpha2Code.ToLower().Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.alpha3Code) && item.alpha3Code.ToLower().Contains(key.ToLower())) ||
+                                    (item.callingCodes.Count > 0 && item.callingCodes.Contains(key.ToLower())) ||
+
+                                    //altSpellings alanı olmayan veriler var
+                                    //(item.altSpellings.Count > 0 && item.altSpellings.Contains(key.ToLower())) ||
+
+                                    (!string.IsNullOrEmpty(item.subregion) && item.subregion.ToLower().Contains(key.ToLower())) ||
+                                    (item.population != 0 && Convert.ToString(item.population) == key) ||
+
+                                    //latlng alanı olmayan veriler var
+                                    //(item.latlng.Count > 0 && item.latlng.Contains(Convert.ToDouble(key)))
+
+                                    (!string.IsNullOrEmpty(item.demonym) && item.demonym.ToLower().Contains(key.ToLower())) ||
+                                    (item.area != 0 && Convert.ToString(item.area) == key) ||
+                                    (item.timezones.Count > 0 && item.timezones.Contains(key.ToLower())) ||
+
+                                    //borders alanı olmayan veriler var
+                                    //(item.borders.Count > 0 && item.borders.Contains(key.ToLower())) ||
+
+                                    (!string.IsNullOrEmpty(item.nativeName) && item.nativeName.ToLower().Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.numericCode) && item.numericCode.Contains(key)) ||
+                                    (!string.IsNullOrEmpty(item.flags.png) && item.flags.png.ToLower().Contains(key.ToLower())) ||
+                                    (!string.IsNullOrEmpty(item.flags.svg) && item.flags.svg.ToLower().Contains(key.ToLower()))
+
+                                     //Bu kısımdaki arama kodlarını yazmakta zorlandım. Hem Currencies hemde dizi şeklinde olması zorlanma sebebim oldu.
+                                     //(!item.currencies.Where(a => a.name.ToLower().Contains(key.ToLower())).Select(a=>a.name).Any())
+
+                                     //Bu kısımdaki arama kodlarını yazmakta zorlandım. Hem Language hemde dizi şeklinde olması zorlanma sebebim oldu.
+                                     //(!item.languages.Where(a => a.name.ToLower().Contains(key.ToLower())).Select(a=>a.name).Any())
+
+                                     
+
                               select item).ToList();
                 return filter;
             }
@@ -62,7 +90,7 @@ namespace UICoreMVC.APIService.Concrete
         {
             var response = await _httpClient.GetAsync("https://restcountries.com/v2/all");
             var data = await response.Content.ReadAsStringAsync();
-            
+
             if (response.IsSuccessStatusCode)
             {
                 var myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(data);
@@ -70,6 +98,6 @@ namespace UICoreMVC.APIService.Concrete
             }
             return null;
         }
-      
+
     }
 }
