fc0411783cac2c1aa5b7352d65c8aa1fe02090cb
erolcan54 <erolcan@live.com>
2022-06-21 12:37:19 +0300

    21/06/2022 12:37 Update

--- UICoreMVC/APIService/Concrete/RootService.cs
+++ UICoreMVC/APIService/Concrete/RootService.cs
@@ -27,7 +27,7 @@ namespace UICoreMVC.APIService.Concrete
             {
                 var myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(data);
                 var filter = (from item in myDeserializedClass.ToList()
-                    where !string.IsNullOrEmpty(item.capital) && item.capital.Contains(key)
+                    where !string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())
                     select item).ToList();
                 return filter;
             }
@@ -43,10 +43,16 @@ namespace UICoreMVC.APIService.Concrete
             {
                 var myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(data);
                 var filter = (from item in myDeserializedClass
-                    where (!string.IsNullOrEmpty(item.capital) && item.capital.Contains(key)) ||
-                          (!string.IsNullOrEmpty(item.name) && item.name.Contains(key)) ||
-                          (!string.IsNullOrEmpty(item.region) && item.region.Contains(key))
-                    select item).ToList();
+                    where (!string.IsNullOrEmpty(item.capital) && item.capital.ToLower().Contains(key.ToLower())) ||
+                          (!string.IsNullOrEmpty(item.name) && item.name.ToLower().Contains(key.ToLower())) ||
+                          (!string.IsNullOrEmpty(item.region) && item.region.ToLower().Contains(key.ToLower())) ||
+                          (item.topLevelDomain.Count>0 && item.topLevelDomain.Contains(key.ToLower())) ||
+                          (!string.IsNullOrEmpty(item.alpha2Code) && item.alpha2Code.ToLower().Contains(key.ToLower())) ||
+                          (!string.IsNullOrEmpty(item.alpha3Code) && item.alpha3Code.ToLower().Contains(key.ToLower())) ||
+                          (item.callingCodes.Count > 0 && item.callingCodes.Contains(key.ToLower()))||
+                          (item.altSpellings.Count > 0 && item.altSpellings.Contains(key.ToLower())) ||
+                          (!string.IsNullOrEmpty(item.subregion) && item.subregion.ToLower().Contains(key.ToLower()))
+                              select item).ToList();
                 return filter;
             }
             return null;
